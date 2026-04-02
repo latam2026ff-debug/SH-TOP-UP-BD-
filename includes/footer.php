eeb4804121c3b629fc3582383c757de9c9bce14c
latam2026ff-debug <latam2026ff@gmail.com>
2026-04-03 01:29:52 +0600

    Add footer.php file with footer content and JavaScript includes

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<!-- Footer content -->
+<footer>
+    <p>&copy; 2026 My Website. All rights reserved.</p>
+</footer>
+
+<!-- Include JavaScript files -->
+<script src="path/to/your/script1.js"></script>
+<script src="path/to/your/script2.js"></script>
